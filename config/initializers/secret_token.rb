8c74c5235715e558030efc00ce9b14acd624c6d7
iwabuchiken <iwabuchi.k.2010@gmail.com>
2012-05-10 13:29:46 +0900

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LearnChineseHelper::Application.config.secret_token = 'd00f2397ec21ce539ad65660c390d777facd629a8c2e5efd31ae50b96f5d43c8f4332977b139fef439010abcde0a6c53698796187c7d9eed609db9a5e10bed45'
